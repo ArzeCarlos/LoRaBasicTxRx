09882920f30ae9fda60c7744cf2f37a724f3beab
ArzeCarlos <carlosarzez25@gmail.com>
2023-10-10 09:33:33 -0400

    Comunicación MQTT broker ESP-IDF deserialize JSON

---
+++ Timer_Interrupt/Timer_interrupt_MQTT_JSON/main/main.c
@@ -0,0 +1,223 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+#include "esp_wifi.h"
+#include "esp_system.h"
+#include "esp_timer.h"
+#include "nvs_flash.h"
+#include "esp_event.h"
+#include "esp_netif.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "freertos/queue.h"
+#include "freertos/event_groups.h"
+
+#include "lwip/sockets.h"
+#include "lwip/dns.h"
+#include "lwip/netdb.h"
+
+#include "esp_log.h"
+#include "mqtt_client.h"
+#include "driver/gpio.h"
+
+#include "cJSON.h"
+static const char *TAG = "WIFI-CONNECTION";
+static const char *TAG2 = "JSON";
+static const char *TAG1 = "MQTT-CONNECTION";
+#define  ESP_WIFI_SSID "FAMILIA MEJIA"
+#define  ESP_WIFI_PASS "LALA8787bebe"
+#define ESP_MAXIMUM_RETRY  10
+static EventGroupHandle_t s_wifi_event_group;
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT      BIT1
+static int s_retry_num = 0;
+uint32_t MQTT_CONNECTED = 0;
+esp_timer_handle_t timer_handler;
+
+static void mqtt_app_start(void);
+static void event_handler(void* arg, esp_event_base_t event_base,
+                                int32_t event_id, void* event_data)
+{
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        esp_wifi_connect();
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        if (s_retry_num < ESP_MAXIMUM_RETRY) {
+            esp_wifi_connect();
+            s_retry_num++;
+            ESP_LOGI(TAG, "retry to connect to the AP");
+        } else {
+            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+        }
+        ESP_LOGI(TAG,"connect to the AP fail");
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+        ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+        s_retry_num = 0;
+        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+    }
+}
+void wifi_init(void)
+{
+    s_wifi_event_group = xEventGroupCreate();
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+    esp_event_handler_instance_t instance_any_id;
+    esp_event_handler_instance_t instance_got_ip;
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        ESP_EVENT_ANY_ID,
+                                                        &event_handler,
+                                                        NULL,
+                                                        &instance_any_id));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
+                                                        IP_EVENT_STA_GOT_IP,
+                                                        &event_handler,
+                                                        NULL,
+                                                        &instance_got_ip));
+
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = ESP_WIFI_SSID,
+            .password = ESP_WIFI_PASS,
+            .scan_method = WIFI_FAST_SCAN,
+            .threshold.authmode = WIFI_AUTH_WPA2_PSK,
+        },
+    };
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config) );
+    ESP_ERROR_CHECK(esp_wifi_start() );
+    ESP_LOGI(TAG, "wifi_init_sta finished.");
+    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+            WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+            pdFALSE,
+            pdFALSE,
+            portMAX_DELAY);
+    if (bits & WIFI_CONNECTED_BIT) {
+        ESP_LOGI(TAG, "connected to ap SSID:%s password:%s",
+                 ESP_WIFI_SSID, ESP_WIFI_PASS);
+    } else if (bits & WIFI_FAIL_BIT) {
+        ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s",
+                 ESP_WIFI_SSID, ESP_WIFI_PASS);
+    } else {
+        ESP_LOGE(TAG, "UNEXPECTED EVENT");
+    }
+    ESP_ERROR_CHECK(esp_event_handler_instance_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, instance_got_ip));
+    ESP_ERROR_CHECK(esp_event_handler_instance_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, instance_any_id));
+    vEventGroupDelete(s_wifi_event_group);
+}
+static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
+{
+    ESP_LOGD(TAG1, "Event dispatched from event loop base=%s, event_id=%" PRIi32 "", base, event_id);
+    esp_mqtt_event_handle_t event = event_data;
+    esp_mqtt_client_handle_t client = event->client;
+    int msg_id;
+    switch ((esp_mqtt_event_id_t)event_id)
+    {
+    case MQTT_EVENT_CONNECTED:
+        ESP_LOGI(TAG1, "MQTT_EVENT_CONNECTED");
+        MQTT_CONNECTED=1;
+        msg_id = esp_mqtt_client_subscribe(client, "test", 0);
+        ESP_LOGI(TAG1, "sent subscribe successful, msg_id=%d", msg_id);
+        msg_id = esp_mqtt_client_subscribe(client, "test2", 0);
+        ESP_LOGI(TAG1, "sent subscribe successful, msg_id=%d", msg_id);
+        break;
+    case MQTT_EVENT_DISCONNECTED:
+        ESP_LOGI(TAG1, "MQTT_EVENT_DISCONNECTED");
+        MQTT_CONNECTED=0;
+        break;
+
+    case MQTT_EVENT_SUBSCRIBED:
+        ESP_LOGI(TAG1, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
+        break;
+    case MQTT_EVENT_UNSUBSCRIBED:
+        ESP_LOGI(TAG1, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
+        break;
+    case MQTT_EVENT_PUBLISHED:
+        ESP_LOGI(TAG1, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
+        break;
+    case MQTT_EVENT_DATA:
+        char topic_str[6];
+        char velgatewayStr[6];
+        char velnode1Str[6];
+        ESP_LOGI(TAG, "MQTT_EVENT_DATA");
+        printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
+        printf("DATA=%.*s\r\n", event->data_len, event->data);
+        sprintf(topic_str,"%.*s", event->topic_len, event->topic);
+        if(!(strcmp(topic_str,"test2"))){
+            cJSON *root = cJSON_Parse((char *)event->data);
+            if (root == NULL) {
+            ESP_LOGE(TAG2, "Failed to parse JSON data");
+            }
+            else{
+                ESP_LOGI(TAG2, "Deserialize.....");
+                if (cJSON_GetObjectItem(root, "velgateway")) {
+                    char *velgateway = cJSON_GetObjectItem(root,"velgateway")->valuestring; 
+                    sprintf(velgatewayStr,"%s",velgateway);            
+	            }
+                if (cJSON_GetObjectItem(root, "velnode1")) {
+                    char *velnode1 = cJSON_GetObjectItem(root,"velnode1")->valuestring;
+                    sprintf(velnode1Str,"%s",velnode1);                 
+	            }
+                ESP_LOGI(TAG2, "velgateway=%s    velnode1=%s",velgatewayStr,velnode1Str);
+                cJSON_Delete(root);
+                ESP_ERROR_CHECK(esp_timer_stop(timer_handler));
+                ESP_ERROR_CHECK(esp_timer_start_periodic(timer_handler,atoi(velgatewayStr)*1000000));
+            } 
+        }
+        break;
+    case MQTT_EVENT_ERROR:
+        ESP_LOGI(TAG1, "MQTT_EVENT_ERROR");
+        break;
+    default:
+        ESP_LOGI(TAG1, "Other event id:%d", event->event_id);
+        break;
+    }
+}
+esp_mqtt_client_handle_t client = NULL;
+static void mqtt_app_start(void)
+{
+    ESP_LOGI(TAG1, "STARTING MQTT");
+    esp_mqtt_client_config_t mqttConfig = {
+        .broker.address.uri = "mqtt://192.168.0.8",
+        .broker.address.port = 1883,
+        .credentials.username = "test",
+        .credentials.client_id= "esp-client-1",
+        .credentials.authentication.password ="12345"};
+    client = esp_mqtt_client_init(&mqttConfig);
+    esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, client);
+    esp_mqtt_client_start(client);
+}
+void timer_callback(void *param)
+{
+  static bool ON;
+  ON = !ON;
+  esp_mqtt_client_publish(client, "test", "Hello World", 0, 0, 0);
+  gpio_set_level(GPIO_NUM_2, ON);
+}
+void app_main(void)
+{
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+    wifi_init();
+    mqtt_app_start();
+    const esp_timer_create_args_t my_timer_args = {
+      .callback = &timer_callback,
+      .name = "My Timer"};
+    gpio_set_direction(GPIO_NUM_2, GPIO_MODE_OUTPUT);
+    ESP_ERROR_CHECK(esp_timer_create(&my_timer_args, &timer_handler));
+    ESP_ERROR_CHECK(esp_timer_start_periodic(timer_handler, 10000000));
+    while (true)
+    {
+        esp_timer_dump(stdout);
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
